c7f1ad0a281d32648bb7c86c13c2e4baf9735554
B. Drawe <brycerdrawe.b@gmail.com>
2020-09-26 13:27:13 -0600

    prompts work

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
-// array of questions for user
-const questions = [
+const fs = require('fs');
 
-];
+const inquirer = require('inquirer');
+
+// import { prompt } from 'inquirer';
+
+// array of questions for user
+// const questions = () => {
+inquirer
+     .prompt ([
+        {
+            type:'input',
+            name: 'projName',
+            message: 'What is your projects name?'
+        },
+        {
+            type:'input',
+            name: 'description',
+            message: 'Please give a detailed description of your project'   
+        },
+        {
+            type:'input',
+            name: 'installation',
+            message: 'If any, please provide installation instructions'
+        },
+        {
+            type:'input',
+            name: 'useInfo',
+            message: 'Please provide instruction/examples on how this App would be used.'
+        },
+        {
+            type:'input',
+            name: 'contributor',
+            message: 'Please list the developers that helped in the project.'
+        }
+            
+    ])
+// }
+
 
 // function to write README file
 function writeToFile(fileName, data) {
